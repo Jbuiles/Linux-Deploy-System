d16f2c4b983e8d023e01529345d126d0eb813553
Jbuiles <35709632+Jbuiles@users.noreply.github.com>
2018-05-11 00:21:02 -0400

    Update config.php

--- Deploy/Development/config.php
+++ Deploy/Development/config.php
@@ -1,5 +1,5 @@
 <?php
-$Version= "1_1";
-$Server= "DMZ";
-$Path = "/home/orchestration/Orchestration/$Server/$Server$Version.tar.gz";
+$Version= "1_1";#Your version number here
+$Server= "DMZ";#Your server name here
+$Path = "/home/orchestration/Orchestration/$Server/$Server$Version.tar.gz";#Your path where you will save the files on the Orchestration Server
 ?>

f9d02f9bc54aadfc128ef4c20db9d8931d3d3b8c
Jbuiles <35709632+Jbuiles@users.noreply.github.com>
2018-05-11 00:29:49 -0400

    Update Deploy.php

--- Deploy/Development/Deploy.php
+++ Deploy/Development/Deploy.php
@@ -1,16 +1,17 @@
 <?php
-//Get Linux Current User
-#$md= get_current_user ();
 $processUser = posix_getpwuid(posix_geteuid());//gets process owner
 $User= $processUser['name'];
-include "/home/$User/git/it490/config.php";
+$Folder="";#Set this to where your application is installed
+include ""; //Link the path to where your config.php file is located
+
+$servername = "";//Set to ip of Orchestration Server
+$username = ""; //Set the username of your database
+$password = ""; //Set the password for your username
+$dbname=""; //Set the database name
+
 
 //Connect To Database
-$servername = "192.168.2.2";//Set to ip of Orchestration Server
-$username = "dev";
-$password = "12345";
-$dbname="packages";
-
+//This code gets the Version,Server and path values from the database and appends them as a file name for the packages you want sent
 try {
 	$con = new PDO ("mysql:host=$servername;dbname=$dbname", $username, $password);
 	$sql = "INSERT INTO packages (version,server,path) VALUES (:Version, :Server, :Path)";
@@ -21,7 +22,7 @@ try {
 	$result->execute();#Write Version/Server/Path to sql table
 
 	$filename ="$Server$Version.tar.gz";#Setup format for File name
-	shell_exec("cd /home/$User/git && tar -cvzf $filename it490");#Zip Folder
+	shell_exec("cd /home/$User/git && tar -cvzf $filename $Folder);#Zips up Folder
 	shell_exec("cd /home/$User/git && scp $filename orchestration@$servername:Orchestration/$Server"); #Copy Zip to Orchestration
 	shell_exec("php /home/$User/Documents/pass.php"); #Sends Update notice to Orchestration
 }
